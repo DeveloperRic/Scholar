1c3f464b1360eefaa3fab4d279b89b7f8a790e88
Victor <olaitanvictor99@gmail.com>
2021-06-05 14:13:51 -0400

    Protect manage route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] },
   // TODO AuthGuard connects to Auth0 not Realm so it requires a redirect, code a custom one to prevent redirects
-  { path: 'manage', loadChildren: () => import('./manage/manage.module').then(m => m.ManageModule) },
+  { path: 'manage', canActivate: [AuthGuard], loadChildren: () => import('./manage/manage.module').then(m => m.ManageModule) },
   { path: 'login/callback', component: LoginCallbackComponent }
   // TODO add a 404 page
 ]
@@ -20,4 +20,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
