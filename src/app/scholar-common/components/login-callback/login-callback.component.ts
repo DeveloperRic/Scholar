17f7aa4170604dd46e2d8931d4d61a49d862bfaf
Victor <olaitanvictor99@gmail.com>
2021-07-03 13:52:47 -0400

    Remove relaod from login callback

--- src/app/scholar-common/components/login-callback/login-callback.component.ts
+++ src/app/scholar-common/components/login-callback/login-callback.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
-
 import { AuthService } from '@auth0/auth0-angular'
-
-import { RealmService } from '../../../database/realm.service'
+import { RealmService } from 'src/app/database/realm.service'
 
 @Component({
   selector: 'app-login-callback',
@@ -11,7 +8,11 @@ import { RealmService } from '../../../database/realm.service'
   styleUrls: ['./login-callback.component.css']
 })
 export class LoginCallbackComponent implements OnInit {
-  constructor(private router: Router, private authService: AuthService, private realmService: RealmService) {}
+
+  constructor(
+    private authService: AuthService,
+    private realmService: RealmService
+  ) { }
 
   ngOnInit(): void {
     console.log('LoginCallbackComponent: Waiting for Auth0 JWT...')
@@ -22,13 +23,7 @@ export class LoginCallbackComponent implements OnInit {
       if (!this.realmService.isLoggedIn()) {
         throw new Error('LoginCallbackButtonComponent: Login confirmation failed, realmService.isLoggedIn() returned false')
       }
-      console.log('LoginCallbackButtonComponent: Successfully logged in, reloading page...')
-      const path = window.location.pathname
-      if (path == '/') {
-        window.location.reload()
-      } else {
-        this.router.navigateByUrl(window.location.pathname, { replaceUrl: true })
-      }
+      console.log('LoginCallbackButtonComponent: Successfully logged in')
     })
   }
 }
